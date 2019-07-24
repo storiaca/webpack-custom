7fe2a2012b9071583d726f95e1fa69047a178d6c
Aleksandar Ristic <storiaca@gmail.com>
2019-07-24 11:59:48 +0200

    Add html-loader, file-loader and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
@@ -21,6 +16,20 @@ module.exports = {
           "css-loader", // 2. Turns css into commonjs
           "sass-loader" // 1. Turns sass into css
         ]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
       }
     ]
   }
