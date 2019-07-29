af871f54a48bd7d1d30982192eb5279852689742
Aleksandar Ristic <storiaca@gmail.com>
2019-07-29 10:23:55 +0200

    Install babel-preset-react, react and react-dom

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(.js?x)$/,
+        test: /\.(.js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
       },
