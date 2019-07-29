6368b95c3b094c6d8064fc04eb3a82b76632b2ef
Aleksandar Ristic <storiaca@gmail.com>
2019-07-29 12:39:22 +0200

    Create App component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "./main.scss";
 
-const title = "React with Webpack and Babel and";
-
-ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 module.hot.accept();
