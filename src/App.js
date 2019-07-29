81524a15052ad320f38ba99346f7507209400a98
Aleksandar Ristic <storiaca@gmail.com>
2019-07-29 12:44:46 +0200

    Add bootstrap layout in component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>React with Webpack and Babel</h1>
+        <div className="row">
+          <div className="col-md-6">Left div</div>
+          <div className="col-md-6">Right div</div>
+        </div>
       </div>
     );
   }
